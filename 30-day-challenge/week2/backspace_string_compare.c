b5904a9d532b8b7f11fa0314b4ef8ced56aeb96c
simulacr4m <ssharma10393@gmail.com>
2020-04-09 22:45:14 -0500

    Added optimized solution with O(N) time and O(1) space

--- 30-day-challenge/week2/backspace_string_compare.c
+++ 30-day-challenge/week2/backspace_string_compare.c
@@ -1,3 +1,5 @@
+/* Less Efficient Solution */
+
 bool backspaceCompare(char * S, char * T) {
     int i = 0, k = 0;
     char *cpyS = calloc(1, strlen(S)+1);
@@ -33,4 +35,34 @@ bool backspaceCompare(char * S, char * T) {
     return comparison;
 }
 
+/* More Efficient solution */
+
+bool backspaceCompare(char * S, char * T) {
+    int i = 0, k = 0;
+    for (i = 0; i < strlen(S); i++) {
+        if (i > 0 && S[i] == '#') {
+            for (k = i+1; k <= strlen(S); k++) {
+                S[k-2] = S[k];
+                S[k] = ' ';
+            }
+            i -= 2;
+        }
+    }
+    for (i = 0; i < strlen(T); i++) {
+        if (i > 0 && T[i] == '#') {
+            for (k = i+1; k <= strlen(T); k++) {
+                T[k-2] = T[k];
+                T[k] = ' ';
+            }
+            i -= 2;
+        }
+    }
+    for (; *S != '\0' && (*S == '#' || *S == ' '); *S++);
+    for (; *T != '\0' && (*T == '#' || *T == ' '); *T++);
+    for (i = strlen(S)-1; i >= 0 && (S[i] == '#' || S[i] == ' '); i--);
+    S[i+1] = '\0';
+    for (i = strlen(T)-1; i >= 0 && (T[i] == '#' || T[i] == ' '); i--);
+    T[i+1] = '\0';
+    return !strcmp(S, T);
+}
 

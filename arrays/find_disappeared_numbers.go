5191b48b6c3828912c248fa3b204feb1bb4404ec
simulacr4m <ssharma10393@gmail.com>
2020-05-10 15:54:25 -0500

    Added a more efficient solution to Find Disappeared Number exercise

--- arrays/find_disappeared_numbers.go
+++ arrays/find_disappeared_numbers.go
@@ -11,3 +11,23 @@ func findDisappearedNumbers(nums []int) []int {
     }
     return disappeared
 }
+
+/* More efficient solution */
+
+func findDisappearedNumbers(nums []int) []int {
+    var disappeared []int
+    for i := 0; i < len(nums); i++ {
+        for nums[i] != 0 && nums[nums[i]-1] != 0 {
+            index := nums[i]-1
+            nums[i] = nums[index]
+            nums[index] = 0
+        }
+    }
+    
+    for i := 0; i < len(nums); i++ {
+        if nums[i] > 0 {
+            disappeared = append(disappeared, i+1)
+        }
+    }
+    return disappeared
+}
